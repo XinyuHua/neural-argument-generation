a7ed3ab91b7dfab56a88352df494c10459877708
Xinyu <xinyuhua1994@gmail.com>
2018-07-30 09:55:45 -0400

    add initial file structure

---
+++ src/data_loader.py
@@ -0,0 +1,20 @@
+# Author: Xinyu Hua <hua.x@husky.neu.edu>
+# Last modified: 2018-07-30
+
+from random import shuffle
+import tensorflow as tf
+import time
+import os
+import sys
+import numpy as np
+
+class DataSample(object):
+
+    def __init__(self):
+        return
+
+
+class Batch(object):
+
+    def __init__(self):
+        return
---
+++ src/exp_train_test.py
@@ -0,0 +1,5 @@
+# Author: Xinyu Hua <hua.x@husky.neu.edu>
+# Last modified: 2018-07-30
+
+import tensorflow as tf
+
