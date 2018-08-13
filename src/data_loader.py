23a55addaeba75a035d400cc15244bbf1c3278ca
xinyu hua <xinyu@xinyus-MacBook-Pro.local>
2018-08-13 15:52:17 -0400

    build file structure

--- src/data_loader.py
+++ src/data_loader.py
@@ -2,19 +2,247 @@
 # Last modified: 2018-07-30
 
 from random import shuffle
+import Queue
 import tensorflow as tf
 import time
 import os
 import sys
+import utils
 import numpy as np
 
 class DataSample(object):
 
-    def __init__(self):
+    def __init__(self, src_txt, tgt_txt, src_vocab, tgt_vocab, hps):
+        """
+        Args:
+            src_txt: tokenized string of source input, each token is separated by one single space.
+            tgt_txt: 
+            src_vocab: Vocabulary for source input.
+            tgt_vocab: Vocabulary for target output.
+            hps: hyperparameters.
+        """
+        self.hps = hps
+
+        start_decoding = tgt_vocab.word2id()
+        stop_decoding = tgt_vocab.word2id()
+
+        # tokenize the source input
+        src_lst = src_txt.split()
+        if len(src_lst) > self.hps.max_enc_steps:
+            src_lst = src_lst[: self.hps.max_enc_steps]
+        self.enc_lens = len(src_lst)
+        self.enc_input_wids = [src_vocab.word2id(tok) for tok in src_lst]
+
+        # tokenize the target output
+        tgt_str = ' '.join(tgt_txt)
+        tgt_lst = tgt_str.split()
+        tgt_ids = [tgt_vocab.word2id(tok) for tok in tgt_lst]
+
+        self.dec_input_wids, self.target = self.get_dec_inp_targ_seqs(tgt_ids, hps.max_dec_steps, start_decoding, stop_decoding)
+        self.dec_lens = len(self.dec_input_wids)
+
+        self.original_src = src_txt
+        self.original_tgt = tgt_str
+        self.original_tgt_sents = tgt_txt
+
         return
+
+    def get_dec_inp_targ_seqs(self, sequence, max_len, start_id, stop_id):
+        """Given the reference summary as a sequence of tokens, return the input sequence for the decoder, and the target sequence which we will use to calculate loss. The sequence will be truncated if it is longer than max_len. The input sequence must start with the start_id and the target sequence must end with the stop_id (but not if it's been truncated).
+
+        Args:
+          sequence: List of ids (integers)
+          max_len: integer
+          start_id: integer
+          stop_id: integer
+
+        Returns:
+          inp: sequence length <=max_len starting with start_id
+          target: sequence same length as input, ending with stop_id only if there was no truncation
+        """
+        inp = [start_id] + sequence[:]
+        target = sequence[:]
+        if len(inp) > max_len: # truncate
+          inp = inp[:max_len]
+          target = target[:max_len] # no end_token
+        else: # no truncation
+          target.append(stop_id) # end token
+        assert len(inp) == len(target)
+        return inp, target
+
+
+    def pad_decoder_inp_targ(self, max_len, pad_id):
+        """Pad decoder input and target sequences with pad_id up to max_len."""
+        while len(self.dec_input) < max_len:
+            self.dec_input.append(pad_id)
+        while len(self.target) < max_len:
+          self.target.append(pad_id)
+
+
+    def pad_encoder_input(self, max_len, pad_id):
+        """Pad the encoder input sequence with pad_id up to max_len."""
+        while len(self.enc_input) < max_len:
+            self.enc_input.append(pad_id)
+
+
 
 
 class Batch(object):
+  """A class to generate minibatches of data. Buckets examples together based on length of the encoder sequence."""
 
-    def __init__(self):
-        return
+  BATCH_QUEUE_MAX = 100 # max number of batches the batch_queue can hold
+
+  def __init__(self, data_path, src_vocab, tgt_vocab, hps, single_pass):
+    """Initialize the batcher. Start threads that process the data into batches.
+
+    Args:
+      data_path: tf.Example filepattern.
+      vocab: Vocabulary object
+      hps: hyperparameters
+      single_pass: If True, run through the dataset exactly once (useful for when you want to run evaluation on the dev or test set). Otherwise generate random batches indefinitely (useful for training).
+    """
+    self._data_path = data_path
+    self._src_vocab = src_vocab
+    self._tgt_vocab = tgt_vocab
+    self._hps = hps
+    self._single_pass = single_pass
+
+    # Initialize a queue of Batches waiting to be used, and a queue of Examples waiting to be batched
+    self._batch_queue = Queue.Queue(self.BATCH_QUEUE_MAX)
+    self._example_queue = Queue.Queue(self.BATCH_QUEUE_MAX * self._hps.batch_size)
+
+    # Different settings depending on whether we're in single_pass mode or not
+    if single_pass:
+      self._num_example_q_threads = 1 # just one thread, so we read through the dataset just once
+      self._num_batch_q_threads = 1  # just one thread to batch examples
+      self._bucketing_cache_size = 1 # only load one batch's worth of examples before bucketing; this essentially means no bucketing
+      self._finished_reading = False # this will tell us when we're finished reading the dataset
+    else:
+      self._num_example_q_threads = 16 # num threads to fill example queue
+      self._num_batch_q_threads = 4  # num threads to fill batch queue
+      self._bucketing_cache_size = 100 # how many batches-worth of examples to load into cache before bucketing
+
+    # Start the threads that load the queues
+    self._example_q_threads = []
+    for _ in xrange(self._num_example_q_threads):
+      self._example_q_threads.append(Thread(target=self.fill_example_queue))
+      self._example_q_threads[-1].daemon = True
+      self._example_q_threads[-1].start()
+    self._batch_q_threads = []
+    for _ in xrange(self._num_batch_q_threads):
+      self._batch_q_threads.append(Thread(target=self.fill_batch_queue))
+      self._batch_q_threads[-1].daemon = True
+      self._batch_q_threads[-1].start()
+
+    # Start a thread that watches the other threads and restarts them if they're dead
+    if not single_pass: # We don't want a watcher in single_pass mode because the threads shouldn't run forever
+      self._watch_thread = Thread(target=self.watch_threads)
+      self._watch_thread.daemon = True
+      self._watch_thread.start()
+
+
+  def next_batch(self):
+    """Return a Batch from the batch queue.
+
+    If mode='decode' then each batch contains a single example repeated beam_size-many times; this is necessary for beam search.
+
+    Returns:
+      batch: a Batch object, or None if we're in single_pass mode and we've exhausted the dataset.
+    """
+    # If the batch queue is empty, print a warning
+    if self._batch_queue.qsize() == 0:
+      tf.logging.warning('Bucket input queue is empty when calling next_batch. Bucket queue size: %i, Input queue size: %i', self._batch_queue.qsize(), self._example_queue.qsize())
+      if self._single_pass and self._finished_reading:
+        tf.logging.info("Finished reading dataset in single_pass mode.")
+        return None
+
+    batch = self._batch_queue.get() # get the next Batch
+    return batch
+
+  def fill_example_queue(self):
+    """Reads data from file and processes into Examples which are then placed into the example queue."""
+
+    input_gen = self.text_generator(utils.example_generator(self._data_path, self._single_pass))
+
+    while True:
+      try:
+        (article, abstract) = input_gen.next() # read the next example from file. article and abstract are both strings.
+      except StopIteration: # if there are no more examples:
+        tf.logging.info("The example generator for this example queue filling thread has exhausted data.")
+        if self._single_pass:
+          tf.logging.info("single_pass mode is on, so we've finished reading dataset. This thread is stopping!!")
+          self._finished_reading = True
+          break
+        else:
+          raise Exception("single_pass mode is off but the example generator is out of data; error.")
+
+      abstract_sentences = [sent.strip() for sent in utils.abstract2sents(abstract)] # Use the <s> and </s> tags in abstract to get a list of sentences.
+      example = DataSample(article, abstract_sentences, self._src_vocab, self._tgt_vocab, self._hps) # Process into an Example.
+      self._example_queue.put(example) # place the Example in the example queue.
+
+
+  def fill_batch_queue(self):
+    """Takes Examples out of example queue, sorts them by encoder sequence length, processes into Batches and places them in the batch queue.
+
+    In decode mode, makes batches that each contain a single example repeated.
+    """
+    while True:
+      if self._hps.mode != 'decode':
+        # Get bucketing_cache_size-many batches of Examples into a list, then sort
+        inputs = []
+        for _ in xrange(self._hps.batch_size * self._bucketing_cache_size):
+          inputs.append(self._example_queue.get())
+        inputs = sorted(inputs, key=lambda inp: inp.enc_len) # sort by length of encoder sequence
+
+        # Group the sorted Examples into batches, optionally shuffle the batches, and place in the batch queue.
+        batches = []
+        for i in xrange(0, len(inputs), self._hps.batch_size):
+          batches.append(inputs[i:i + self._hps.batch_size])
+        if not self._single_pass:
+          shuffle(batches)
+        for b in batches:  # each b is a list of Example objects
+          self._batch_queue.put(Batch(b, self._hps, self._src_vocab.word2id(utils.PAD_TOKEN), self._tgt_vocab.word2id(utils.PAD_TOKEN)))
+
+      else: # beam search decode mode
+        ex = self._example_queue.get()
+        b = [ex for _ in xrange(self._hps.batch_size)]
+        self._batch_queue.put(Batch(b, self._hps, self._src_vocab.word2id(utils.PAD_TOKEN), self._tgt_vocab.word2id(utils.PAD_TOKEN)))
+
+
+  def watch_threads(self):
+    """Watch example queue and batch queue threads and restart if dead."""
+    while True:
+      time.sleep(60)
+      for idx,t in enumerate(self._example_q_threads):
+        if not t.is_alive(): # if the thread is dead
+          tf.logging.error('Found example queue thread dead. Restarting.')
+          new_t = Thread(target=self.fill_example_queue)
+          self._example_q_threads[idx] = new_t
+          new_t.daemon = True
+          new_t.start()
+      for idx,t in enumerate(self._batch_q_threads):
+        if not t.is_alive(): # if the thread is dead
+          tf.logging.error('Found batch queue thread dead. Restarting.')
+          new_t = Thread(target=self.fill_batch_queue)
+          self._batch_q_threads[idx] = new_t
+          new_t.daemon = True
+          new_t.start()
+
+
+  def text_generator(self, example_generator):
+    """Generates article and abstract text from tf.Example.
+
+    Args:
+      example_generator: a generator of tf.Examples from file. See data.example_generator"""
+    while True:
+      e = example_generator.next() # e is a tf.Example
+      try:
+        article_text = e.features.feature['article'].bytes_list.value[0] # the article text was saved under the key 'article' in the data files
+        abstract_text = e.features.feature['abstract'].bytes_list.value[0] # the abstract text was saved under the key 'abstract' in the data files
+      except ValueError:
+        tf.logging.error('Failed to get article or abstract from example')
+        continue
+      if len(article_text)==0: # See https://github.com/abisee/pointer-generator/issues/1
+        tf.logging.warning('Found an example with empty article text. Skipping it.')
+      else:
+        yield (article_text, abstract_text)
---
+++ src/utils.py
@@ -0,0 +1,321 @@
+import tensorflow as tf
+import time
+import os
+import glob
+import random
+import struct
+import csv
+import numpy as np
+from tensorflow.core.example import example_pb2
+
+
+FLAGS = tf.app.flags.FLAGS
+
+def get_config():
+  """Returns config for tf.session"""
+  config = tf.ConfigProto(allow_soft_placement=True)
+  config.gpu_options.allow_growth=True
+  return config
+
+def load_ckpt(saver, sess, ckpt_dir="train", ckpt_id=None):
+  """Load checkpoint from the ckpt_dir (if unspecified, this is train dir) and restore it to saver and sess, waiting 10 secs in the case of failure. Also returns checkpoint name."""
+  while True:
+    try:
+      if ckpt_id is None or ckpt_id == -1 :
+        latest_filename = "checkpoint_best" if ckpt_dir=="eval" else None
+        ckpt_dir = os.path.join(FLAGS.log_root, ckpt_dir)
+        ckpt_state = tf.train.get_checkpoint_state(ckpt_dir, latest_filename=latest_filename)
+        tf.logging.info('Loading checkpoint %s', ckpt_state.model_checkpoint_path)
+        ckpt_path = ckpt_state.model_checkpoint_path
+      else:
+        ckpt_path = os.path.join(FLAGS.log_root, ckpt_dir, "model.ckpt-" + str(ckpt_id))
+      saver.restore(sess, ckpt_path)
+      return ckpt_path
+    except:
+      tf.logging.info("Failed to load checkpoint from %s. Sleeping for %i secs...", ckpt_dir, 10)
+      time.sleep(10)
+
+# <s> and </s> are used in the data files to segment the abstracts into sentences. They don't receive vocab ids.
+SENTENCE_START = '<s>'
+SENTENCE_END = '</s>'
+
+PAD_TOKEN = '[PAD]' # This has a vocab id, which is used to pad the encoder input, decoder input and target sequence
+UNKNOWN_TOKEN = '[UNK]' # This has a vocab id, which is used to represent out-of-vocabulary words
+START_DECODING = '[START]' # This has a vocab id, which is used at the start of every decoder input sequence
+STOP_DECODING = '[STOP]' # This has a vocab id, which is used at the end of untruncated target sequences
+
+# Note: none of <s>, </s>, [PAD], [UNK], [START], [STOP] should appear in the vocab file.
+
+
+class Vocab(object):
+  """Vocabulary class for mapping between words and ids (integers)"""
+
+  def __init__(self, vocab_file, max_size):
+    """Creates a vocab of up to max_size words, reading from the vocab_file. If max_size is 0, reads the entire vocab file.
+
+    Args:
+      vocab_file: path to the vocab file, which is assumed to contain "<word> <frequency>" on each line, sorted with most frequent word first. This code doesn't actually use the frequencies, though.
+      max_size: integer. The maximum size of the resulting Vocabulary."""
+    self._word_to_id = {}
+    self._id_to_word = {}
+    self._count = 0 # keeps track of total number of words in the Vocab
+
+    # [UNK], [PAD], [START] and [STOP] get the ids 0,1,2,3.
+    for w in [UNKNOWN_TOKEN, PAD_TOKEN, START_DECODING, STOP_DECODING]:
+      self._word_to_id[w] = self._count
+      self._id_to_word[self._count] = w
+      self._count += 1
+
+    # Read the vocab file and add words up to max_size
+    with open(vocab_file, 'r') as vocab_f:
+      for line in vocab_f:
+        pieces = line.strip().split()
+        # if len(pieces) != 2:
+        #   print 'Warning: incorrectly formatted line in vocabulary file: %s\n' % line
+        #   continue
+        w = pieces[0]
+        if w in [SENTENCE_START, SENTENCE_END, UNKNOWN_TOKEN, PAD_TOKEN, START_DECODING, STOP_DECODING]:
+          raise Exception('<s>, </s>, [UNK], [PAD], [START] and [STOP] shouldn\'t be in the vocab file, but %s is' % w)
+        if w in self._word_to_id:
+          raise Exception('Duplicated word in vocabulary file: %s' % w)
+        self._word_to_id[w] = self._count
+        self._id_to_word[self._count] = w
+        self._count += 1
+        if max_size != 0 and self._count >= max_size:
+          print "max_size of vocab was specified as %i; we now have %i words. Stopping reading." % (max_size, self._count)
+          break
+
+    print "Finished constructing vocabulary of %i total words. Last word added: %s" % (self._count, self._id_to_word[self._count-1])
+    self.load_stopwords()
+    print "top 10 words:"
+    for i in range(10):
+      print(i, self._id_to_word[i])
+
+  def word2id(self, word):
+    """Returns the id (integer) of a word (string). Returns [UNK] id if word is OOV."""
+    if word not in self._word_to_id:
+      return self._word_to_id[UNKNOWN_TOKEN]
+    return self._word_to_id[word]
+
+  def id2word(self, word_id):
+    """Returns the word (string) corresponding to an id (integer)."""
+    if word_id not in self._id_to_word:
+      raise ValueError('Id not found in vocab: %d' % word_id)
+    return self._id_to_word[word_id]
+
+  def load_stopwords(self):
+    self.stopword_wids = set()
+    for line in open("/data/xinyu/general_corpus/stop_words.txt"):
+      token = line.strip()
+      if token in self._word_to_id:
+        self.stopword_wids.add(self._word_to_id[token])
+    return
+
+  def size(self):
+    """Returns the total size of the vocabulary"""
+    return self._count
+
+  def write_metadata(self, fpath):
+    """Writes metadata file for Tensorboard word embedding visualizer as described here:
+      https://www.tensorflow.org/get_started/embedding_viz
+
+    Args:
+      fpath: place to write the metadata file
+    """
+    print "Writing word embedding metadata file to %s..." % (fpath)
+    with open(fpath, "w") as f:
+      fieldnames = ['word']
+      writer = csv.DictWriter(f, delimiter="\t", fieldnames=fieldnames)
+      for i in xrange(self.size()):
+        writer.writerow({"word": self._id_to_word[i]})
+
+
+def example_generator(data_path, single_pass):
+  """Generates tf.Examples from data files.
+
+    Binary data format: <length><blob>. <length> represents the byte size
+    of <blob>. <blob> is serialized tf.Example proto. The tf.Example contains
+    the tokenized article text and summary.
+
+  Args:
+    data_path:
+      Path to tf.Example data files. Can include wildcards, e.g. if you have several training data chunk files train_001.bin, train_002.bin, etc, then pass data_path=train_* to access them all.
+    single_pass:
+      Boolean. If True, go through the dataset exactly once, generating examples in the order they appear, then return. Otherwise, generate random examples indefinitely.
+
+  Yields:
+    Deserialized tf.Example.
+  """
+  while True:
+    filelist = glob.glob(data_path) # get the list of datafiles
+    assert filelist, ('Error: Empty filelist at %s' % data_path) # check filelist isn't empty
+    if single_pass:
+      filelist = sorted(filelist)
+      print(filelist)
+    else:
+      random.shuffle(filelist)
+    for f in filelist:
+      reader = open(f, 'rb')
+      while True:
+        len_bytes = reader.read(8)
+        if not len_bytes: break # finished reading this file
+        str_len = struct.unpack('q', len_bytes)[0]
+        example_str = struct.unpack('%ds' % str_len, reader.read(str_len))[0]
+        yield example_pb2.Example.FromString(example_str)
+    if single_pass:
+      print "example_generator completed reading all datafiles. No more data."
+      break
+
+
+def article2ids(article_words, vocab):
+  """Map the article words to their ids. Also return a list of OOVs in the article.
+
+  Args:
+    article_words: list of words (strings)
+    vocab: Vocabulary object
+
+  Returns:
+    ids:
+      A list of word ids (integers); OOVs are represented by their temporary article OOV number. If the vocabulary size is 50k and the article has 3 OOVs, then these temporary OOV numbers will be 50000, 50001, 50002.
+    oovs:
+      A list of the OOV words in the article (strings), in the order corresponding to their temporary article OOV numbers."""
+  ids = []
+  oovs = []
+  unk_id = vocab.word2id(UNKNOWN_TOKEN)
+  for w in article_words:
+    i = vocab.word2id(w)
+    if i == unk_id: # If w is OOV
+      if w not in oovs: # Add to list of OOVs
+        oovs.append(w)
+      oov_num = oovs.index(w) # This is 0 for the first article OOV, 1 for the second article OOV...
+      ids.append(vocab.size() + oov_num) # This is e.g. 50000 for the first article OOV, 50001 for the second...
+    else:
+      ids.append(i)
+  return ids, oovs
+
+
+def abstract2ids(abstract_words, vocab, article_oovs):
+  """Map the abstract words to their ids. In-article OOVs are mapped to their temporary OOV numbers.
+
+  Args:
+    abstract_words: list of words (strings)
+    vocab: Vocabulary object
+    article_oovs: list of in-article OOV words (strings), in the order corresponding to their temporary article OOV numbers
+
+  Returns:
+    ids: List of ids (integers). In-article OOV words are mapped to their temporary OOV numbers. Out-of-article OOV words are mapped to the UNK token id."""
+  ids = []
+  unk_id = vocab.word2id(UNKNOWN_TOKEN)
+  for w in abstract_words:
+    i = vocab.word2id(w)
+    if i == unk_id: # If w is an OOV word
+      if w in article_oovs: # If w is an in-article OOV
+        vocab_idx = vocab.size() + article_oovs.index(w) # Map to its temporary article OOV number
+        ids.append(vocab_idx)
+      else: # If w is an out-of-article OOV
+        ids.append(unk_id) # Map to the UNK token id
+    else:
+      ids.append(i)
+  return ids
+
+
+def outputids2words(id_list, vocab, article_oovs):
+  """Maps output ids to words, including mapping in-article OOVs from their temporary ids to the original OOV string (applicable in pointer-generator mode).
+
+  Args:
+    id_list: list of ids (integers)
+    vocab: Vocabulary object
+    article_oovs: list of OOV words (strings) in the order corresponding to their temporary article OOV ids (that have been assigned in pointer-generator mode), or None (in baseline mode)
+
+  Returns:
+    words: list of words (strings)
+  """
+  words = []
+  for i in id_list:
+    try:
+      w = vocab.id2word(i) # might be [UNK]
+    except ValueError as e: # w is OOV
+      assert article_oovs is not None, "Error: model produced a word ID that isn't in the vocabulary. This should not happen in baseline (no pointer-generator) mode"
+      article_oov_idx = i - vocab.size()
+      try:
+        w = article_oovs[article_oov_idx]
+      except ValueError as e: # i doesn't correspond to an article oov
+        raise ValueError('Error: model produced word ID %i which corresponds to article OOV %i but this example only has %i article OOVs' % (i, article_oov_idx, len(article_oovs)))
+    words.append(w)
+  return words
+
+def outputids2words_withcopy(id_list, vocab, attn_dists, ref, show=True):
+    ref = ref.split()
+    if len(attn_dists[0]) == 2:
+        attn_dists= zip(*attn_dists)[0]
+    assert len(attn_dists) == len(id_list)
+
+    words = []
+    for ind,i in enumerate(id_list):
+        try:
+          w = vocab.id2word(i) # might be [UNK]
+        except ValueError as e: # w is OOV
+          raise ValueError("Error: model produced a word ID that isn't in the vocabulary.")
+        if w == UNKNOWN_TOKEN:
+            copy = np.argmax(attn_dists[ind])
+            w = ref[copy]
+            if show:
+                w = "__%s__" % w
+        words.append(w)
+    return words
+
+def abstract2sents(abstract):
+  """Splits abstract text from datafile into list of sentences.
+
+  Args:
+    abstract: string containing <s> and </s> tags for starts and ends of sentences
+
+  Returns:
+    sents: List of sentence strings (no tags)"""
+  cur = 0
+  sents = []
+  while True:
+    try:
+      start_p = abstract.index(SENTENCE_START, cur)
+      end_p = abstract.index(SENTENCE_END, start_p + 1)
+      cur = end_p + len(SENTENCE_END)
+      sents.append(abstract[start_p+len(SENTENCE_START):end_p])
+    except ValueError as e: # no more sentences
+      return sents
+
+
+def show_art_oovs(article, vocab):
+  """Returns the article string, highlighting the OOVs by placing __underscores__ around them"""
+  unk_token = vocab.word2id(UNKNOWN_TOKEN)
+  words = article.split(' ')
+  words = [("__%s__" % w) if vocab.word2id(w)==unk_token else w for w in words]
+  out_str = ' '.join(words)
+  return out_str
+
+
+def show_abs_oovs(abstract, vocab, article_oovs):
+  """Returns the abstract string, highlighting the article OOVs with __underscores__.
+
+  If a list of article_oovs is provided, non-article OOVs are differentiated like !!__this__!!.
+
+  Args:
+    abstract: string
+    vocab: Vocabulary object
+    article_oovs: list of words (strings), or None (in baseline mode)
+  """
+  unk_token = vocab.word2id(UNKNOWN_TOKEN)
+  words = abstract.split(' ')
+  new_words = []
+  for w in words:
+    if vocab.word2id(w) == unk_token: # w is oov
+      if article_oovs is None: # baseline mode
+        new_words.append("__%s__" % w)
+      else: # pointer-generator mode
+        if w in article_oovs:
+          new_words.append("__%s__" % w)
+        else:
+          new_words.append("!!__%s__!!" % w)
+    else: # w is in-vocab word
+      new_words.append(w)
+  out_str = ' '.join(new_words)
+  return out_str
